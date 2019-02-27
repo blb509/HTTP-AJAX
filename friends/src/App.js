7257286702818e4ed6cc561e63c131f2281e676f
Brandon L. Borrero <borrero509@outlook.com>
2019-02-27 14:02:39 -0800

    Got data from server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: [],
+      err: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log('CDM now running');
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        console.log(res);
+        this.setState({ friendsList: res.data});
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ err: err });
+      });
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>hi</div>
   }
 }
 
